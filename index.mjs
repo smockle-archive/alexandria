556b1813b8c469183d90ad7d62692ce4f33f16ef
Clay Miller <clay@smockle.com>
2017-11-18 00:42:37 -0800

    Hello world with express

---
+++ index.mjs
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+// When I save a URL to Instapaper,
+//  - IFTTT “Instapaper” condition is triggered, so “Maker” event is triggered
+//  - Alexandria extracts a “domain” from a “url” in IFTTT’s request body
+//  - Alexandria calls the Pocket “get” API endpoint providing “domain”
+//  - Pocket responds with a list of items — each includes a “given_url” and “resovled_url” parameter
+//  - Alexandria extracts a list of “given_url” and “resolved_url”
+//  - Alexandria stops processing if the “url” is listed — this signifies an already-queued item
+//  - Alexandria calls the Pocket “add” API endpoint providing “url”
+//  - Pocket responds by queueing the item at the URL
+
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(3000, () => console.log("Listening on port 3000"));
