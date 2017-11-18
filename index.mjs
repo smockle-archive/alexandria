f2bf34d5d35390e8da4536e343adb47435e90092
Clay Miller <clay@smockle.com>
2017-11-18 01:09:38 -0800

    Obtain request token

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+//@ts-check
 
 // When I save a URL to Instapaper,
 //  - IFTTT “Instapaper” condition is triggered, so “Maker” event is triggered
@@ -10,9 +11,60 @@
 //  - Alexandria calls the Pocket “add” API endpoint providing “url”
 //  - Pocket responds by queueing the item at the URL
 
+import dotenv from "dotenv";
+dotenv.config();
+
+import https from "https";
 import express from "express";
+
 const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+function getRequestToken(consumerKey, redirectURI) {
+  const postData = JSON.stringify({
+    consumer_key: consumerKey,
+    redirect_uri: redirectURI
+  });
+  const options = {
+    hostname: "api.getpocket.com",
+    port: "443",
+    path: "/v3/oauth/request",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=UTF8",
+      "Content-Length": postData.length,
+      "X-Accept": "application/json"
+    }
+  };
+  var request = https.request(options, response => {
+    console.log("statusCode:", response.statusCode);
+    console.log("headers:", response.headers);
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+    response.on("data", data => {
+      process.stdout.write(data);
+    });
+  });
+  request.on("error", error => {
+    console.error(error);
+  });
+  request.write(postData);
+  request.end();
+}
+
+app.get("/", (request, response) => {
+  setTimeout(
+    () =>
+      getRequestToken(
+        process.env.POCKET_CONSUMER_KEY,
+        `http://localhost:${port}/authorization-finished`
+      ),
+    0
+  );
+  response.send("Hello World!");
+});
+
+app.get("/authorization-finished", (request, response) =>
+  response.send(request.body.code)
+);
+
+app.listen(port, () => console.log("Listening on port 3000"));
